f6a2bbf0d7cd68a85c34b61ced98c935212ffd66
Dario A Lencina-Talarico <darioalessandrolencina@gmail.com>
2022-11-21 00:08:13 -0500

    Add CI (#10)
    Co-authored-by: Dario Lencina <dario@securityunion.dev>

--- types/src/lib.rs
+++ types/src/lib.rs
@@ -1,8 +1,7 @@
-
 use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize, PartialEq, Debug, Clone)]
 #[serde(rename_all = "camelCase")]
 pub struct HelloResponse {
-    pub name: String
+    pub name: String,
 }

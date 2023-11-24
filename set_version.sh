51a0345a4db219f4b786336e32856766d541e8b3
Dario Lencina <dario@securityunion.dev>
2023-11-24 18:29:11 -0500

    set version

---
+++ set_version.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Specify the new version
+NEW_VERSION="1.0.0"
+
+# List of directories containing Cargo.toml files
+CRATES=("types" "actix-api" "yew-ui")
+
+# Loop through each crate and update the version
+for CRATE in "${CRATES[@]}"; do
+    echo "Updating version for $CRATE to $NEW_VERSION"
+    sed -i '' "s/^version = \".*\"/version = \"$NEW_VERSION\"/" "$CRATE/Cargo.toml"
+done
+
+echo "Version update complete."

a2b9cd315a77d8cc4fa431f3e3e7c818498d938e
Dario Lencina <dario@securityunion.dev>
2023-11-24 22:47:38 -0500

    got tests to pass

--- actix-api/tests/server.rs
+++ actix-api/tests/server.rs
@@ -2,6 +2,7 @@ mod common;
 use actix_api::get_app;
 use actix_web::test;
 
+use types::HelloResponse;
 /// Test login inserts pkce_challenge, pkce_verifier, csrf_state
 /// And returns a login url with the pkce_challenge
 ///
@@ -12,8 +13,17 @@ async fn test_login() {
     println!("DB_URL: {}", db_url);
     common::dbmate_up(&db_url);
     let mut app = test::init_service(get_app()).await;
-    let req = test::TestRequest::get().uri("/login").to_request();
+    let req = test::TestRequest::get().uri("/hello/dario").to_request();
     let resp = test::call_service(&mut app, req).await;
-    drop(app);
-    assert!(resp.status() == 302);
+    assert_eq!(resp.status(), 200);
+    // Get body as json
+    let body = test::read_body(resp).await;
+    let body = String::from_utf8(body.to_vec()).unwrap();
+    let body: HelloResponse = serde_json::from_str(&body).unwrap();
+    assert_eq!(
+        body,
+        HelloResponse {
+            name: "dario".to_string()
+        }
+    );
 }
